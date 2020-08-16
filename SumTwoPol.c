fefd6a1c76b6f0056929f173af1dd76979390f20
dinhhh <dinhhung.0115@gmail.com>
2020-08-16 22:15:22 +0700

    Added my project

--- SumTwoPol.c
+++ SumTwoPol.c
@@ -18,13 +18,16 @@ node *createNode(int coeff, int pow){
 node *createPol(){
     int answer;
     node *head = (node *)malloc(sizeof(node));
+    // get input
     printf("Enter first: coeff = ");
     scanf("%d", &head->coeff);
     printf("pow = ");
     scanf("%d", &head->pow);
     head->next = NULL;
-    do
-    {
+    // loop 
+    printf("continued? (yes = 1 / no = 0)  ");
+    scanf("%d", &answer);
+    while (answer == 1){
         //temp == last node
         node *temp = head;
         while (temp->next != NULL)
@@ -42,13 +45,15 @@ node *createPol(){
         // add another?
         printf("continued? (yes = 1 / no = 0)  ");
         scanf("%d", &answer);
-    } while (answer == 1);
+    }
     printf("Create successful!!!\n");
     return head;
 }
 
 void printList(node *head){
     node *temp = head;
+    if(temp == NULL)
+        printf("List is empty!!!\n");
     while (temp != NULL)
     {
         printf("Coeff = %d   ", temp->coeff);
@@ -71,6 +76,7 @@ node *addToLast(node *head, int coeff, int pow){
     return head;
 }
 
+// ham tinh tong 2 bieu thuc
 node *sumTwoPol(node *p1, node *p2){
     node *res = (node *)malloc(sizeof(node));
     node *t1 = p1;
@@ -127,6 +133,40 @@ node *sumTwoPol(node *p1, node *p2){
     return res;
 }
 
+// ham tinh tich 2 bieu thuc
+node *multTwoPol(node *p1, node *p2){
+    node *t1 = p1;
+    
+    // create first node in result
+    node *res = (node *)malloc(sizeof(node));
+    if(res == NULL){
+        printf("Out of memory!!!\n");
+        return res;
+    }
+    res->coeff = 0;
+    res->pow = 0;
+    res->next = NULL;
+    while (t1 != NULL){
+        node *r = (node *)malloc(sizeof(node));
+        if(r == NULL){
+            printf("Out of memory!!!\n");
+            return NULL;
+        }
+        r->coeff = 0;
+        r->pow = 0;
+        r->next = NULL;
+        node *t2 = p2;
+        while (t2 != NULL){
+            r = addToLast(r, t1->coeff * t2->coeff, t1->pow + t2->pow);
+            t2 = t2->next;
+        }
+        res = sumTwoPol(res, r);
+        t1 = t1->next;
+    }
+    // remove the first node (0, 0) in res
+    res = res->next;
+    return res;
+}
 int main(){
     node *p1 = createPol();
     node *p2 = createPol();
@@ -137,11 +177,9 @@ int main(){
     printf("result:\n");
     
     node *res = sumTwoPol(p1, p2);
+    printf("Sum two pol:\n");
     printList(res);
-    /*
-    addToLast(p1, 100, 100);
-    printf("p1 after add:\n");
-    printList(p1);
-    */
+    node *mul = multTwoPol(p1, p2);
+    printf("Mul two pol:\n");
+    printList(mul);
 }
-// FUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKkkkkkkk
